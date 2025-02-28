371be917d1f623af5580d9adca2f2f01b1766778
timmPrice <timmPrice6@gmail.com>
2025-02-28 10:21:35 -0500

    -g --get flags work to GET the file... I want to add descriptive pathing where users can set which directory to put there stuff in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import readCookie, getPuzzle, parse_puzzle
+from src import readCookie, getPuzzle
 import argparse
 
 def main():
@@ -6,8 +6,18 @@ def main():
     parser = argparse.ArgumentParser(
         prog='aoc',
         description='gather aoc puzzle info and input',
-        epilog='\/\/\/\/\/\/'
+        epilog='that gone do it'
     )
+
+    parser.add_argument("-g", "--get", nargs=2, metavar=("YEAR", "DAY"), type=int,
+                        help = "Get puzzle input for a given year and day"
+                       )
+
+    args = parser.parse_args()
+
+    if args.get:
+        year, day = args.get
+        getPuzzle(year, day, cookie)
 
 if __name__ == "__main__":
     main()
